88f5863046b6da1c5082d12393fcaa3e803bf7ae
Prashant Vaghani <vaghani.prashant77@gmail.com>
2022-09-20 15:24:36 +0200

    Ros Package of custom message and Robot Arm

---
+++ Code_Cpp/camera.cpp
@@ -0,0 +1,29 @@
+#include <opencv\opencv.hpp>
+
+using namespace cv;
+
+using namespace std;
+
+int main() {
+
+Mat image;
+
+namedWindow("Display window");
+
+VideoCapture cap(0);
+
+if (!cap.isOpened()) {
+
+cout << "cannot open camera";
+
+}
+}
+
+while (true) {
+
+cap >> image;
+
+imshow("Display window", image);
+
+waitKey(25);
+}
